f5dc1992704a80d0386f65924c5ea6066c62cb43
Chris-Harkies <christopher.harkies@gmail.com>
2025-07-05 13:17:43 -0700

    DB connection working

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const db = require('./src/DB');
 
+db.query('Create TABLE orders (orderId             INT IDENTITY,    orderDate           DATETIME,    totalAmount         DECIMAL(10,2),    shiptoAddress       VARCHAR(50),    shiptoCity          VARCHAR(40),    shiptoState         VARCHAR(20),    shiptoPostalCode    VARCHAR(20),    shiptoCountry       VARCHAR(40),    customerId          INT,    PRIMARY KEY (orderId)');
+
 const PORT = 3300;
 
+
+
 app.get('/', (req, res) => {
-    res.send('Back End ');
+    res.send(db.query('SELECT *'));
 })
 
 app.listen(PORT, () => {
--- src/DB.js
+++ src/DB.js
@@ -2,7 +2,7 @@ const {Pool} = require('pg');
 require('dotenv').config();
 const pool = new Pool ({
     user: process.env.DB_USER,
-    host: process.env.HOST,
+    host: 'db-postgresql-nyc3-42107-do-user-23698589-0.g.db.ondigitalocean.com',
     database: process.env.DATABASE,
     password:process.env.DB_p,
     ports:process.env.PORTS
