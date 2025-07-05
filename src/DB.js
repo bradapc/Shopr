a64729a5488ee09c49792173401674c7b6af61eb
Chris-Harkies <christopher.harkies@gmail.com>
2025-07-05 12:53:21 -0700

    db updates

--- src/DB.js
+++ src/DB.js
@@ -1,26 +1,15 @@
-const { Client } = pkg;
 const {Pool} = require('pg');
 require('dotenv').config();
-const DB = async () => {
-  const client = new Client({
-        user: process.env.DB_USER,
+const pool = new Pool ({
+    user: process.env.DB_USER,
     host: process.env.HOST,
     database: process.env.DATABASE,
     password:process.env.DB_p,
     ports:process.env.PORTS
   });
 
-  try {
-    await client.connect();
-    console.log('Connected to PostgreSQL database!');
-    // Example query
-    const res = await client.query('SELECT NOW()');
-    console.log('Current time:', res.rows[0]);
-  } catch (err) {
-    console.error('Error connecting to the database:', err);
-  } finally {
-    await client.end();
-  }
-};
 
-modules.export = DB;
+module.exports = {
+    query: (text, params) => pool.query(text, params),
+    pool
+};
