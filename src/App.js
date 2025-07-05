3c364a466a3ebcfb68d6cf9230a6998ffe2e55e4
bradapc <brad.cocar@gmail.com>
2025-07-05 11:32:06 -0700

    Added front end links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import {Route, Routes, BrowserRouter, Link} from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
+      <h1 className="rootHeader">Welcome to Shopr</h1>
+      <BrowserRouter>
+          <Link to="/shop" className="rootLink">Begin Shopping</Link>
+          <Link to="/orders" className="rootLink">View all Orders</Link>
+      </BrowserRouter>
     </div>
   );
 }
