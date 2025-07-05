831c7141a13da146f28c635a3388c908de60ad09
Chris-Harkies <christopher.harkies@gmail.com>
2025-07-05 13:21:10 -0700

    DB connection

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const db = require('./src/DB');
 
-db.query('Create TABLE orders (orderId             INT IDENTITY,    orderDate           DATETIME,    totalAmount         DECIMAL(10,2),    shiptoAddress       VARCHAR(50),    shiptoCity          VARCHAR(40),    shiptoState         VARCHAR(20),    shiptoPostalCode    VARCHAR(20),    shiptoCountry       VARCHAR(40),    customerId          INT,    PRIMARY KEY (orderId)');
+
 
 const PORT = 3300;
 
